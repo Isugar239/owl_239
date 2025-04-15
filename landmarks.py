f51b569cf675a90bc23765b7486b3ffc47ffe891
Isugar239 <za1van@yandex.ru>
2025-04-15 19:48:18 +0300

    modified:   landmarks.py 	modified:   tte.py

--- landmarks.py
+++ landmarks.py
@@ -1,9 +1,8 @@
-
 import cv2
 import numpy as np
 import mediapipe as mp
-import tensorflow as tf
 from tensorflow.keras.models import load_model
+
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
