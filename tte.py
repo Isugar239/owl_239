2abb2ee426f72b2917616d1f09c055e200598c04
Ivan <163177546+Isugar239@users.noreply.github.com>
2025-04-21 11:53:32 +0300

    Update tte.py

--- tte.py
+++ tte.py
@@ -12,81 +12,40 @@ pd.set_option("display.max_colwidth", None)
 with open("data.txt", "r", encoding="utf-8") as f:
         text = f.read()
 
-RAW_KNOWLEDGE_BASE = [
-    LangchainDocument(page_content=text, metadata={"source": "data.txt"})
-   ]
-
-EMBEDDING_MODEL_NAME = "ai-forever/sbert_large_nlu_ru"
-MARKDOWN_SEPARATORS = [
-    "\n#{1,6} ",
-    "```\n",
-    "\n\\*\\*\\*+\n",
-    "\n---+\n",
-    "\n___+\n",
-    "\n\n",
-    "\n",
-    " ",
-    "",
-]
-
-
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,  # The maximum number of characters in a chunk: we selected this value arbitrarily
-    chunk_overlap=100,  # The number of characters to overlap between chunks
-    add_start_index=True,  # If `True`, includes chunk's start index in metadata
-    strip_whitespace=True,  # If `True`, strips whitespace from the start and end of every document
-    separators=MARKDOWN_SEPARATORS,
+df = pd.DataFrame.from_dict(
+    [
+        {
+            "x": documents_projected[i, 0],
+            "y": documents_projected[i, 1],
+            "source": docs_processed[i].metadata["source"].split("/")[1],
+            "extract": docs_processed[i].page_content[:100] + "...",
+            "symbol": "circle",
+            "size_col": 4,
+        }
+        for i in range(len(docs_processed))
+    ]
+    + [
+        {
+            "x": documents_projected[-1, 0],
+            "y": documents_projected[-1, 1],
+            "source": "User query",
+            "extract": user_query,
+            "size_col": 100,
+            "symbol": "star",
+        }
+    ]
 )
 
-docs_processed = []
-for doc in RAW_KNOWLEDGE_BASE:
-    docs_processed += text_splitter.split_documents([doc])
-EMBEDDING_MODEL_NAME = "thenlper/gte-small"
-
-
-def split_documents(
-    chunk_size: int,
-    knowledge_base: List[LangchainDocument],
-    tokenizer_name: Optional[str] = EMBEDDING_MODEL_NAME,
-) -> List[LangchainDocument]:
-    """
-    Split documents into chunks of maximum size `chunk_size` tokens and return a list of documents.
-    """
-    text_splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
-        AutoTokenizer.from_pretrained(tokenizer_name),
-        chunk_size=chunk_size,
-        chunk_overlap=int(chunk_size / 10),
-        add_start_index=True,
-        strip_whitespace=True,
-        separators=MARKDOWN_SEPARATORS,
-    )
-
-    docs_processed = []
-    for doc in knowledge_base:
-        docs_processed += text_splitter.split_documents([doc])
-
-    # Remove duplicates
-    unique_texts = {}
-    docs_processed_unique = []
-    for doc in docs_processed:
-        if doc.page_content not in unique_texts:
-            unique_texts[doc.page_content] = True
-            docs_processed_unique.append(doc)
-
-    return docs_processed_unique
-
-
-docs_processed = split_documents(
-    512,  # We choose a chunk size adapted to our model
-    RAW_KNOWLEDGE_BASE,
-    tokenizer_name=EMBEDDING_MODEL_NAME,
+# Visualize the embedding
+fig = px.scatter(
+    df,
+    x="x",
+    y="y",
+    color="source",
+    hover_data="extract",
+    size="size_col",
+    symbol="symbol",
+    color_discrete_map={"User query": "black"},
+    width=1000,
+    height=700,
 )
-
-# Let's visualize the chunk sizes we would have in tokens from a common model
-from transformers import AutoTokenizer
-
-tokenizer = AutoTokenizer.from_pretrained(EMBEDDING_MODEL_NAME)
-lengths = [len(tokenizer.encode(doc.page_content)) for doc in tqdm(docs_processed)]
-fig = pd.Series(lengths).hist()
-plt.title("Distribution of document lengths in the knowledge base (in count of tokens)")
-plt.show()
