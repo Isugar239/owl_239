708406b5020147c6cca7c2edc3eb30fec1ad1079
Ivan <163177546+Isugar239@users.noreply.github.com>
2025-04-22 17:12:47 +0300

    Update tte.py

--- tte.py
+++ tte.py
@@ -12,40 +12,96 @@ pd.set_option("display.max_colwidth", None)
 with open("data.txt", "r", encoding="utf-8") as f:
         text = f.read()
 
-df = pd.DataFrame.from_dict(
-    [
-        {
-            "x": documents_projected[i, 0],
-            "y": documents_projected[i, 1],
-            "source": docs_processed[i].metadata["source"].split("/")[1],
-            "extract": docs_processed[i].page_content[:100] + "...",
-            "symbol": "circle",
-            "size_col": 4,
-        }
-        for i in range(len(docs_processed))
-    ]
-    + [
-        {
-            "x": documents_projected[-1, 0],
-            "y": documents_projected[-1, 1],
-            "source": "User query",
-            "extract": user_query,
-            "size_col": 100,
-            "symbol": "star",
-        }
-    ]
+RAW_KNOWLEDGE_BASE = [
+    LangchainDocument(page_content=text, metadata={"source": "data.txt"})
+   ]
+
+EMBEDDING_MODEL_NAME = "ai-forever/sbert_large_nlu_ru"
+MARKDOWN_SEPARATORS = [
+    "\n#{1,6} ",
+    "```\n",
+    "\n\\*\\*\\*+\n",
+    "\n---+\n",
+    "\n___+\n",
+    "\n\n",
+    "\n",
+    " ",
+    "",
+]
+
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,  # The maximum number of characters in a chunk: we selected this value arbitrarily
+    chunk_overlap=100,  # The number of characters to overlap between chunks
+    add_start_index=True,  # If `True`, includes chunk's start index in metadata
+    strip_whitespace=True,  # If `True`, strips whitespace from the start and end of every document
+    separators=MARKDOWN_SEPARATORS,
 )
 
-# Visualize the embedding
-fig = px.scatter(
-    df,
-    x="x",
-    y="y",
-    color="source",
-    hover_data="extract",
-    size="size_col",
-    symbol="symbol",
-    color_discrete_map={"User query": "black"},
-    width=1000,
-    height=700,
+docs_processed = []
+for doc in RAW_KNOWLEDGE_BASE:
+    docs_processed += text_splitter.split_documents([doc])
+EMBEDDING_MODEL_NAME = "thenlper/gte-small"
+
+
+def split_documents(
+    chunk_size: int,
+    knowledge_base: List[LangchainDocument],
+    tokenizer_name: Optional[str] = EMBEDDING_MODEL_NAME,
+) -> List[LangchainDocument]:
+    """
+    Split documents into chunks of maximum size `chunk_size` tokens and return a list of documents.
+    """
+    text_splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
+        AutoTokenizer.from_pretrained(tokenizer_name),
+        chunk_size=chunk_size,
+        chunk_overlap=int(chunk_size / 10),
+        add_start_index=True,
+        strip_whitespace=True,
+        separators=MARKDOWN_SEPARATORS,
+    )
+
+    docs_processed = []
+    for doc in knowledge_base:
+        docs_processed += text_splitter.split_documents([doc])
+
+    # Remove duplicates
+    unique_texts = {}
+    docs_processed_unique = []
+    for doc in docs_processed:
+        if doc.page_content not in unique_texts:
+            unique_texts[doc.page_content] = True
+            docs_processed_unique.append(doc)
+
+    return docs_processed_unique
+
+
+from langchain.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores.utils import DistanceStrategy
+
+embedding_model = HuggingFaceEmbeddings(
+    model_name=EMBEDDING_MODEL_NAME,
+    multi_process=True,
+    model_kwargs={"device": "cuda"},
+    encode_kwargs={"normalize_embeddings": True},  # Set `True` for cosine similarity
 )
+
+KNOWLEDGE_VECTOR_DATABASE = FAISS.from_documents(
+    docs_processed, embedding_model, distance_strategy=DistanceStrategy.COSINE
+)
+user_query = input()
+query_vector = embedding_model.embed_query(user_query)
+
+import pacmap
+import numpy as np
+import plotly.express as px
+
+embedding_projector = pacmap.PaCMAP(n_components=2, n_neighbors=None, MN_ratio=0.5, FP_ratio=2.0, random_state=1)
+
+embeddings_2d = [
+    list(KNOWLEDGE_VECTOR_DATABASE.index.reconstruct_n(idx, 1)[0]) for idx in range(len(docs_processed))
+] + [query_vector]
+
+# Fit the data (the index of transformed data corresponds to the index of the original data)
+documents_projected = embedding_projector.fit_transform(np.array(embeddings_2d), init="pca")
